a1949cb1a783a3edd0dc5a0a27416d5334cc43ec
Mathieu Martin <webmat@gmail.com>
2009-05-04 12:56:10 -0400

    Basic tests

--- test/local_config_test.rb
+++ test/local_config_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class LocalConfigTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "with RAILS_ENV set to 'development'" do
+    setup do
+      silence_warnings { ::RAILS_ENV = 'development' }
+    end
+
+    context "in the general case" do
+      setup do
+        @config = LocalConfig.load_config('without_local')
+      end
+
+      should "return the content of the hash for the current environment" do
+        assert !@config.has_key?(:development)
+      end
+
+      should "return a Rails' HashWithIndifferentAccess, for convenience's sake" do
+        assert_equal HashWithIndifferentAccess, @config.class
+      end
+    end
+
+    context "reading with_local.yml" do
+      setup do
+        @config = LocalConfig.load_config('with_local')
+      end
+
+      should "return the values of the .local file" do
+        assert_equal 'local value', @config[:param]
+      end
+    end
+
+    context "reading without_local.yml" do
+      setup do
+        @config = LocalConfig.load_config('without_local')
+      end
+
+      should "return the values of the .yml file" do
+        assert_equal 'never overridden', @config[:param]
+      end
+    end
+  end
+
+  context "reading no_env.yml" do
+    setup do
+      @config = LocalConfig.load_config('no_env', :environment => false)
+    end
+
+    should "return the values of the .yml file" do
+      assert_equal 'value', @config[:param]
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'activesupport'
+
+test_dir = File.dirname(__FILE__)
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(test_dir)
 require 'local_config'
+
+RAILS_ROOT = File.join test_dir, 'test_rails_root'
 
 class Test::Unit::TestCase
 end
